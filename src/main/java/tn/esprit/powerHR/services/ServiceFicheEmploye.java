4f45b2638b642ed5276400a1599d40f60b3f73bb
yassine <yassine.nawara007@gmail.com>
2025-02-12 00:39:37 +0100

    v0

---
+++ src/main/java/tn/esprit/powerHR/services/ServiceFicheEmploye.java
@@ -0,0 +1,30 @@
+package tn.esprit.powerHR.services;
+
+import tn.esprit.powerHR.interfaces.IService;
+import tn.esprit.powerHR.models.FicheEmploye;
+
+import java.util.List;
+
+public class ServiceFicheEmploye implements IService<FicheEmploye> {
+
+
+    @Override
+    public void add(FicheEmploye ficheEmploye) {
+        
+    }
+
+    @Override
+    public void update(FicheEmploye ficheEmploye) {
+
+    }
+
+    @Override
+    public void delete(FicheEmploye ficheEmploye) {
+
+    }
+
+    @Override
+    public List<FicheEmploye> getAll() {
+        return List.of();
+    }
+}
