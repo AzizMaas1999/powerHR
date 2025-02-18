7f46ea78a43e6674278fce309f0d750b273ee74d
azizbaffoun <azizbaffoun67@gmail.com>
2025-02-18 01:32:25 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/powerHr/utils/MyDataBase.java
@@ -0,0 +1,49 @@
+package tn.esprit.powerHr.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDataBase {
+    private static MyDataBase instance;
+    private static Connection connection;
+    
+    private final String URL = "jdbc:mysql://127.0.0.1:3306/maha2";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "";
+
+    private MyDataBase() {
+        try {
+            // Establish connection
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected to Database Successfully!");
+        } catch (SQLException e) {
+            System.err.println("Database Connection Error: " + e.getMessage());
+        }
+    }
+
+    public static MyDataBase getInstance() {
+        if(instance == null) {
+            instance = new MyDataBase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (instance != null && connection != null) {
+            try {
+                connection.close();
+                connection = null;
+                instance = null;
+                System.out.println("Database connection closed.");
+            } catch (SQLException e) {
+                System.err.println("Error closing database connection!");
+                e.printStackTrace();
+            }
+        }
+    }
+}
