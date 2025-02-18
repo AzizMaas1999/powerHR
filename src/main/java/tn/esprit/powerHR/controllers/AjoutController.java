587487431f9a06c6284e931c5edce608dc5207eb
aymen lahmar <aymenlahmar14@gmail.com>
2025-02-18 18:10:50 +0100

    v1.1 SceneBuilder

---
+++ src/main/java/tn/esprit/powerHR/controllers/AjoutController.java
@@ -0,0 +1,20 @@
+package tn.esprit.powerHR.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AjoutController {
+
+    @FXML
+    private TextField tf1;
+
+    @FXML
+    private TextField tf2;
+
+    @FXML
+    private TextField tf3;
+
+    @FXML
+    private TextField tf4;
+
+}
--- src/main/java/tn/esprit/powerHR/test/Main.java
+++ src/main/java/tn/esprit/powerHR/test/Main.java
@@ -17,48 +17,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         MyDataBase.getInstance();
-        //ajouter demandes
 
-//        DemandeSalaireService salaireService = new DemandeSalaireService();
-//        DemandeSalaire demande = new DemandeSalaire(0 ,1, "Augmentation Salaire", 8000);
-//        salaireService.add(demande);
-//
-
-//        DemandeCongeService service = new DemandeCongeService();
-//        Date dateDebut = Date.valueOf("2025-03-01");
-//        Date dateFin = Date.valueOf("2025-03-15");
-//        DemandeConge demandes = new DemandeConge(0,1, "Conges", "hakekda", dateDebut, dateFin);
-//        service.add(demandes);
-
-        //update
-
-//        Date nouvelleDateDebut = Date.valueOf("2017-04-01");
-//        Date nouvelleDateFin = Date.valueOf("2019-03-15");
-//        DemandeConge demandeMiseAJour = new DemandeConge(17, 1, "Conges", "Mals", nouvelleDateDebut, nouvelleDateFin);
-//
-//        service.update(demandeMiseAJour);
-//
-
-
-//}
-//        //getall
-
-//         System.out.println("Liste des demandes de salaire : " + salaireService.getAll());
-//
-//         System.out.println(" Liste des demandes de congé : " + service.getAll());}}
-
-
-        //supp conge
-
-    //   DemandeConge demande = new DemandeConge();
-//        demande.setId(8);
-//        service.delete(demande);
-
-
-        //getall
-//        System.out.println("Liste des demandes : " + service.getAll());
-       // ServiceQuestionnaire serviceQuestionnaire = new ServiceQuestionnaire();
-//
 //        // Ajouter un questionnaire
 //       Date dateCreation = Date.valueOf("2025-03-01");
       // List<RepQuestionnaire> reponses = new ArrayList<>();
@@ -95,27 +54,29 @@ public class Main {
 
 DemandeService demandeService = new DemandeService();
 
-// Création d'un employé fictif (ID = 1) pour l'exemple
-//Employe employe = new Employe(1, "Nom", "Prénom", "Email", "MotDePasse", "Poste", "Département");
-//
-//// Ajout d'une demande de congé
-//Date dateCreation = Date.valueOf("2025-02-18");
-//Date dateDebut = Date.valueOf("2025-03-01");
-//Date dateFin = Date.valueOf("2025-03-15");
-//Demande demandeConge = new Demande(0, dateCreation, "Conges", dateDebut, dateFin, 0, "Besoin de repos", "En attente", employe);
-//        demandeService.add(demandeConge);
-//
-//// Mise à jour d'une demande existante (en supposant que l'ID 1 existe)
-//Date nouvelleDateDebut = Date.valueOf("2025-04-01");
-//Date nouvelleDateFin = Date.valueOf("2025-04-15");
-//Demande demandeMiseAJour = new Demande(1, dateCreation, "Conges", nouvelleDateDebut, nouvelleDateFin, 0, "Raison mise à jour", "Approuvée", employe);
+
+        Employe employe = new Employe(2,null,null,null,null,null,null);
+
+// Ajout d'une demande de congé
+        Date dateCreation = Date.valueOf("2025-02-18");
+        Date dateDebut = Date.valueOf("2025-03-01");
+        Date dateFin = Date.valueOf("2025-03-15");
+        Demande demandeConge = new Demande(0, dateCreation, "Conges", "Besoin de repos", dateDebut, dateFin, employe, "Valider", 0);
+        demandeService.add(demandeConge);
+
+        // Mise à jour d'une demande existante (en supposant que l'ID 1 existe)
+//        Date ndateCreation = Date.valueOf("2027-02-18");
+//        Date nouvelleDateDebut = Date.valueOf("2028-04-01");
+//        Date nouvelleDateFin = Date.valueOf("2028-04-15");
+//        Demande demandeMiseAJour = new Demande(1, ndateCreation, "Conges", "Raison mise à jour", nouvelleDateDebut, nouvelleDateFin, null ,"En Attente", 0);
 //        demandeService.update(demandeMiseAJour);
-//
-//// Affichage de toutes les demandes
-//        System.out.println("Liste des demandes : " + demandeService.getAll());
-//
-//// Suppression d'une demande (en supposant que l'ID 2 existe)
-//Demande demandeASupprimer = new Demande();
+
+        // Affichage de toutes les demandes
+       // System.out.println("Liste des demandes : " + demandeService.getAll());
+
+        // Suppression d'une demande (en supposant que l'ID 2 existe)
+//        Demande demandeASupprimer = new Demande();
 //        demandeASupprimer.setId(2);
 //        demandeService.delete(demandeASupprimer);
-    }}
+    }
+}
