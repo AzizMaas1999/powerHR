cf03b0f15375b2e8aca7b7e4b69482598ae98381
azizbaffoun <azizbaffoun67@gmail.com>
2025-02-26 19:43:01 +0100

    first commit

--- src/main/java/tn/esprit/powerHr/models/Entreprise.java
+++ src/main/java/tn/esprit/powerHr/models/Entreprise.java
@@ -5,14 +5,16 @@ public class Entreprise {
     private String nom;
     private String secteur;
     private String matriculeFiscale;
+    private String email;
 
     // Constructor
     public Entreprise() {}
 
-    public Entreprise(String nom, String secteur, String matriculeFiscale) {
+    public Entreprise(String nom, String secteur, String matriculeFiscale, String email) {
         this.nom = nom;
         this.secteur = secteur;
         this.matriculeFiscale = matriculeFiscale;
+        this.email = email;
     }
 
     // Getters and Setters
@@ -24,6 +26,8 @@ public class Entreprise {
     public void setSecteur(String secteur) { this.secteur = secteur; }
     public String getMatriculeFiscale() { return matriculeFiscale; }
     public void setMatriculeFiscale(String matriculeFiscale) { this.matriculeFiscale = matriculeFiscale; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
@@ -32,6 +36,7 @@ public class Entreprise {
                 ", nom='" + nom + '\'' +
                 ", secteur='" + secteur + '\'' +
                 ", matriculeFiscale='" + matriculeFiscale + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 } 
